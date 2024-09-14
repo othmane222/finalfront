8b7972318dd0a70892d2571433674b2b6a34379f
othmane <othmanemaarad@gmail.com>
2024-09-14 23:03:02 +0100

    fixing pipeline issue 666

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,3 @@
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-transform-private-property-in-object']
-  };
+export const presets = ['@babel/preset-env', '@babel/preset-react'];
+export const plugins = ['@babel/plugin-transform-private-property-in-object'];
   
