43c57264829c30535bcfe695ff9950f455f0c2ba
othmane <othmanemaarad@gmail.com>
2024-09-16 20:32:51 +0100

    qqqaaaaa

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,12 @@ import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(
+  const { container } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>
   );
+  console.log(container.innerHTML); // Add this to inspect output
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
