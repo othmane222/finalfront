4611f8f25c3fc3facb504098dc8bfd79e16c0e2d
othmane <othmanemaarad@gmail.com>
2024-09-16 22:13:05 +0100

    qabb

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-    preset: 'react',
     testEnvironment: 'jsdom',
     testMatch: ['**/?(*.)+(integration.test).[jt]s?(x)'],
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Include any setup files needed
+    setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Adjust this path if necessary
   };
   
